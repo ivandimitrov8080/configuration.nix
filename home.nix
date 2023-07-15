733fb6d65f87aab336954eb9561a0fc4a33b3a11
Ivan Dimitrov <ivan@idimitrov.dev>
2023-07-15 20:33:11 +0300

    git rebase options

--- home.nix
+++ home.nix
@@ -27,6 +27,10 @@
       enable = true;
       userName = "Ivan Dimitrov";
       userEmail = "ivan@idimitrov.dev";
+      extraConfig = {
+        color.ui = "auto";
+        pull.rebase = true;
+      };
     };
     kitty = {
       enable = true;
