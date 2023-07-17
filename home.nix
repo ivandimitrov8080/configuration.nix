875c4657b5dce58ec81ad624b6ac3e592651b0ea
Ivan Dimitrov <ivan@idimitrov.dev>
2023-07-17 10:44:07 +0300

    tmux theme

--- home.nix
+++ home.nix
@@ -45,11 +45,12 @@
       clock24 = true;
       plugins = with pkgs.tmuxPlugins; [
         tilish
+        catppuccin
       ];
       extraConfig = ''
-                set -g default-command "''${SHELL}"
-        	set -g default-terminal "tmux-256color"
-		set -g base-index 1
+                        set -g default-command "''${SHELL}"
+                	set -g default-terminal "tmux-256color"
+        		set -g base-index 1
       '';
     };
     neovim = {
