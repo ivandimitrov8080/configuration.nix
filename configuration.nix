b6270e07fda891237eb43d810b2ec43fff047280
Ivan Dimitrov <ivan@idimitrov.dev>
2023-07-23 12:33:20 +0300

    adding wlr desktop portal

--- configuration.nix
+++ configuration.nix
@@ -32,11 +32,13 @@
 
   security = {
     polkit.enable = true;
+    rtkit.enable = true;
   };
 
   xdg = {
     portal = {
       enable = true;
+      extraPortals = with pkgs;[ xdg-desktop-portal-wlr ];
     };
   };
 
