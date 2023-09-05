ba1ba2bf86d5fa6ced216418a67f3a0830820042
Ivan Dimitrov <ivan@idimitrov.dev>
2023-09-05 18:26:59 +0300

    adding vpsfree stuff

--- sys/mailserver/default.nix
+++ sys/mailserver/default.nix
@@ -1,4 +1,25 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }:
+{
+  imports = [
+    /etc/nixos/vpsadminos.nix
+  ];
+
+  services.openssh.enable = true;
+  services.openssh.settings.PermitRootLogin = "yes";
+  users.extraUsers.root.openssh.authorizedKeys.keys =
+    [
+      ''
+        ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyW157tNiQdeoQsoo5AEzhyi6BvPeqEvChCxCHf3hikmFDqb6bvvlKYb9grW+fqE0HzALRwpXvPKnuUwHKPVG8HZ7NC9bT5RPMO0rFviNoxWF2PNDWG0ivPmLrQGKtCPM3aUIhSdUdlJ7ImYl34KBkUIrSmL7WlLJUvh1PtyyuVfrhpFzFxHwYwVCNO33L89lfl5PY/G9qrjlH64urt/6aWqMdHD8bZ4MHBPcnSwLMd7f0nNa0aTAJMabsfmndZhV24y7T1FUWG0dl27Q4rnpnZJWBDD1IyWIX/aN+DD6eVVWa4tRVJs6ycfw48hft0zs9zLn9mU4a2hxQ6VvfwpqZHOO8XqqOSai9Yw9Ba60iVQokQQiL91KidoSF7zD0U0szdEmylANyAntUcJ1kdu496s21IU2hjYfN/3seH5a9hBk8iPHp/eTeVUXFKh27rRWn0gc+rba1LF0BWfTjRYR7e1uvPEau0I61sNsp3lnMULdkgkZ9rap1sRM6ULlaRXM= ivand@nixos
+      ''
+    ];
+
+  systemd.extraConfig = ''
+    DefaultTimeoutStartSec=900s
+  '';
+
+  time.timeZone = "Europe/Prague";
+
+  system.stateVersion = "23.11";
 
   nix = {
     extraOptions = ''
