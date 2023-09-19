c5cde86c8aa4bdab96bd644c0bf8561b2761e217
Ivan Dimitrov <ivan@idimitrov.dev>
2023-09-19 20:48:24 +0300

    cleanup and upgrade

--- flake.nix
+++ flake.nix
@@ -5,49 +5,32 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    simple-nixos-mailserver = {
-      url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    emacs-overlay = {
-      url = "github:nix-community/emacs-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-doom-emacs = {
-      url = "github:nix-community/nix-doom-emacs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     { self
     , nixpkgs
     , home-manager
-    , simple-nixos-mailserver
-    , emacs-overlay
-    , nix-doom-emacs
     , ...
-    }: {
+    }:
+    let system = "x86_64-linux"; in
+    {
       nixosConfigurations = {
         laptop = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ ./sys/laptop ];
-        };
-        mailserver = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+          inherit system;
           modules = [
-            simple-nixos-mailserver.nixosModule
-            ./sys/mailserver
+            ./sys/laptop
           ];
         };
       };
       homeConfigurations = {
         ivand = home-manager.lib.homeManagerConfiguration {
           extraSpecialArgs = { rootPath = ./.; };
-          modules = [ ./home/laptop nix-doom-emacs.hmModule ];
+          modules = [
+            ./home/laptop
+          ];
           pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            overlays = [ emacs-overlay.overlay ];
+            inherit system;
           };
         };
       };
