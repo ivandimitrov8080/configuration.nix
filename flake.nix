73b4acae137413563f32c1b8b3aff0c0aacee0cd
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-03 16:43:48 +0200

    direct nixpkgs + update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
