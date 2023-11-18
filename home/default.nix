400719f68b04d864e64ace8d87a0c82a582a5c88
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-18 12:55:10 +0200

    refactor of default home

--- home/default.nix
+++ home/default.nix
@@ -1,20 +1,7 @@
 { system, pkgs, home-manager, modules, ... }: {
   ivand = home-manager.lib.homeManagerConfiguration {
     inherit pkgs;
-    modules = [
-      ./ivand
-      modules.packages
-      modules.programs.nvim
-      modules.programs.zsh
-      modules.programs.tmux
-      modules.programs.git
-      modules.programs.chromium
-      modules.programs.kitty
-      modules.programs.lf
-      modules.programs.obs-studio
-      modules.programs.sway
-      modules.programs.swaylock
-    ];
+    modules = with modules.programs; [ nvim zsh tmux git chromium kitty lf obs-studio sway swaylock ] ++ [ ./ivand modules.packages ];
   };
   vid = home-manager.lib.homeManagerConfiguration {
     inherit pkgs;
