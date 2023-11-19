644572643133ae846036ed27cafd0fe02b03d9ce
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-19 07:12:20 +0200

    browserpass enable

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,7 @@
 { system, pkgs, home-manager, modules, ... }: {
   ivand = home-manager.lib.homeManagerConfiguration {
     inherit pkgs;
-    modules = with modules.programs; [ nvim zsh tmux git chromium kitty lf obs-studio sway swaylock ] ++ [ ./ivand modules.packages ];
+    modules = with modules.programs; [ nvim zsh tmux git chromium kitty lf obs-studio sway swaylock browserpass ] ++ [ ./ivand modules.packages ];
   };
   vid = home-manager.lib.homeManagerConfiguration {
     inherit pkgs;
--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -11,5 +11,6 @@
   swaylock = import ./swaylock;
   tmux = import ./tmux { inherit pkgs; };
   zsh = import ./zsh { inherit pkgs; };
+  browserpass = { programs.browserpass.enable = true; };
   sway = import ./sway { inherit pkgs; };
 }
