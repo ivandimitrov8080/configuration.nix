d22690a1b8468916b7d79d94523f46358a38b9ee
Ivan Dimitrov <ivan@idimitrov.dev>
2024-01-29 12:06:42 +0000

    remove starship settings for defaults

--- modules/home/programs/nushell/default.nix
+++ modules/home/programs/nushell/default.nix
@@ -37,13 +37,6 @@
     starship = {
       enable = true;
       enableNushellIntegration = true;
-      settings = {
-        add_newline = true;
-        character = {
-          success_symbol = "[➜](bold green)";
-          error_symbol = "[➜](bold red)";
-        };
-      };
     };
   };
 }
