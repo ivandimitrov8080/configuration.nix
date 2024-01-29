a44bc0b49ff3bdf3e75d6c5482aa5ec3001a6530
Ivan Dimitrov <ivan@idimitrov.dev>
2024-01-29 10:56:19 +0000

    remove carapace completer from nushell

--- modules/home/programs/nushell/default.nix
+++ modules/home/programs/nushell/default.nix
@@ -21,20 +21,12 @@
         }
       '';
       extraConfig = ''
-        let carapace_completer = {|spans|
-        carapace $spans.0 nushell $spans | from json
-        }
         $env.config = {
          show_banner: false,
          completions: {
-         quick: true
-         partial: true
-         algorithm: "fuzzy"
-         external: {
-             enable: true 
-             max_results: 100 
-             completer: $carapace_completer
-           }
+          quick: true
+          partial: true
+          algorithm: "fuzzy"
          }
         }
         $env.PATH = ($env.PATH | split row (char esep) |
