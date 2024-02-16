ecc7afc721094ddf694cd76558cc47bffb36ee53
Ivan Dimitrov <ivan@idimitrov.dev>
2024-02-16 16:52:25 +0200

    better clock

--- modules/home/programs/waybar/default.nix
+++ modules/home/programs/waybar/default.nix
@@ -11,7 +11,13 @@
           "HDMI-A-1"
         ];
         modules-left = [ "sway/workspaces" ];
-        modules-right = [ "battery" "temperature" "clock" ];
+        modules-right = [ "battery" "clock" ];
+
+        clock = {
+          format = "{:%H:%M:%S}";
+          interval = 1;
+          timezones = [ "Europe/Sofia" "Europe/Prague" ];
+        };
 
         "sway/workspaces" = {
           disable-scroll = true;
