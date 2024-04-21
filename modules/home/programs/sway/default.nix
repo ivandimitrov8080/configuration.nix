2788dd7cd06e87b772f792f6f1d7331f15dd684d
Ivan Dimitrov <ivan@idimitrov.dev>
2024-04-21 16:51:49 +0300

    config sway with nix

--- modules/home/programs/sway/default.nix
+++ modules/home/programs/sway/default.nix
@@ -2,7 +2,33 @@
   wayland.windowManager.sway = {
     enable = true;
     systemd.enable = true;
-    config = null;
-    extraConfig = builtins.readFile ./config;
+    config = rec {
+      menu = "rofi -show run";
+      terminal = "kitty";
+      modifier = "Mod4";
+      startup = [
+        { command = "swaymsg 'workspace 1; exec kitty'"; }
+        { command = "swaymsg 'workspace 2; exec firefox'"; }
+      ];
+      bars = [ ];
+      window.titlebar = false;
+      keybindings = pkgs.lib.mkOptionDefault {
+        "${modifier}+p" = "exec ${menu}";
+        "${modifier}+Shift+r" = "reload";
+        "${modifier}+Shift+c" = "kill";
+        "Alt+Shift+l" = "exec ${pkgs.swaylock}/bin/swaylock";
+        "Alt+Shift+t" = "output 'eDP-1' toggle";
+      };
+      input = {
+        "*" = {
+          xkb_layout = "us,bg";
+          xkb_options = "grp:win_space_toggle";
+          xkb_variant = ",phonetic";
+        };
+      };
+    };
+    swaynag = {
+      enable = true;
+    };
   };
 }
