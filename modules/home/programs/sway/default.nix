cde05713f0fd77a74d3697f745a02778385874dc
Ivan Dimitrov <ivan@idimitrov.dev>
2024-04-21 17:07:00 +0300

    add audio keybindings

--- modules/home/programs/sway/default.nix
+++ modules/home/programs/sway/default.nix
@@ -18,6 +18,10 @@
         "${modifier}+Shift+c" = "kill";
         "Alt+Shift+l" = "exec ${pkgs.swaylock}/bin/swaylock";
         "Alt+Shift+t" = "output 'eDP-1' toggle";
+        "XF86AudioMicMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+        "XF86AudioRaiseVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
+        "XF86AudioLowerVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
+        "XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
       };
       input = {
         "*" = {
