5427f04faed36dac06ca19f1a72f7d9758e4feae
Ivan Dimitrov <ivan@idimitrov.dev>
2024-05-15 19:21:13 +0300

    waybar style cleanup

--- modules/home/programs/waybar/default.nix
+++ modules/home/programs/waybar/default.nix
@@ -3,30 +3,8 @@
     enable = true;
     catppuccin.enable = true;
     settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-        height = 30;
-        output = [
-          "eDP-1"
-          "HDMI-A-1"
-        ];
-        modules-left = [ "sway/workspaces" ];
-        modules-center = [ "clock#week" "clock#year" "clock#time" ];
-        modules-right = [ "network" "pulseaudio" "memory" "cpu" "battery" ];
-
-        "clock#time" = {
-          format = "{:%H:%M:%S}";
-          interval = 1;
-        };
-
-        "clock#week" = {
-          format = "{:%a}";
-        };
-
-        "clock#year" = {
-          format = "{:%Y-%m-%d}";
-          tooltip-format = "<tt><small>{calendar}</small></tt>";
+      mainBar =
+        let
           calendar = {
             mode = "year";
             mode-mon-col = 3;
@@ -39,51 +17,82 @@
               today = "<span color='#ff6699'><b><u>{}</u></b></span>";
             };
           };
-        };
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+        in
+        {
+          layer = "top";
+          position = "top";
+          height = 30;
+          output = [
+            "eDP-1"
+            "HDMI-A-1"
+          ];
+          modules-left = [ "sway/workspaces" ];
+          modules-center = [ "clock#week" "clock#year" "clock#time" ];
+          modules-right = [ "network" "pulseaudio" "memory" "cpu" "battery" ];
 
-        battery = {
-          format = "{icon} {capacity}% {time}";
-          format-time = " {H} h {M} m";
-          format-icons = [ "" "" "" "" "" ];
-          states = {
-            warning = 30;
-            critical = 15;
+          "clock#time" = {
+            format = "{:%H:%M:%S}";
+            interval = 1;
+            tooltip-format = tooltip-format;
+            calendar = calendar;
+          };
+
+          "clock#week" = {
+            format = "{:%a}";
+            tooltip-format = tooltip-format;
+            calendar = calendar;
+          };
+
+          "clock#year" = {
+            format = "{:%Y-%m-%d}";
+            tooltip-format = tooltip-format;
+            calendar = calendar;
+          };
+
+          battery = {
+            format = "{icon} {capacity}% {time}";
+            format-time = " {H} h {M} m";
+            format-icons = [ "" "" "" "" "" ];
+            states = {
+              warning = 30;
+              critical = 15;
+            };
+          };
+
+          cpu = {
+            format = "<span color='#4f46e5'></span>  {usage}%";
+          };
+
+          memory = {
+            format = "<span color='#7c3aed'></span>  {percentage}%";
+            interval = 5;
+          };
+
+          pulseaudio = {
+            format = "<span color='#16a34a'>{icon}</span> {volume}% | {format_source}";
+            format-muted = "<span color='#b91c1c'>󰝟</span> {volume}% | {format_source}";
+            format-source = "{volume}% <span color='#16a34a'></span>";
+            format-source-muted = "{volume}% <span color='#b91c1c'></span>";
+            format-icons = {
+              headphone = "";
+              default = [ "" "" "" ];
+            };
+          };
+
+          network = {
+            format-ethernet = "<span color='#06b6d4'>󰈁</span> | <span color='#ea580c'></span> {bandwidthUpBytes}  <span color='#ea580c'></span> {bandwidthDownBytes}";
+            format-wifi = "<span color='#06b6d4'>{icon}</span> | <span color='#ea580c'></span> {bandwidthUpBytes}  <span color='#ea580c'></span> {bandwidthDownBytes}";
+            format-disconnected = "<span color='#b91c1c'>󰈂</span>";
+            format-icons = [ "󰤟" "󰤢" "󰤥" "󰤨" ];
+            interval = 5;
+          };
+
+          "sway/workspaces" = {
+            disable-scroll = true;
+            all-outputs = true;
           };
         };
-
-        cpu = {
-          format = "<span color='#4f46e5'></span>  {usage}%";
-        };
-
-        memory = {
-          format = "<span color='#7c3aed'></span>  {percentage}%";
-          interval = 5;
-        };
-
-        pulseaudio = {
-          format = "<span color='#16a34a'>{icon}</span> {volume}% | {format_source}";
-          format-muted = "<span color='#b91c1c'>󰝟</span> {volume}% | {format_source}";
-          format-source = "{volume}% <span color='#16a34a'></span>";
-          format-source-muted = "{volume}% <span color='#b91c1c'></span>";
-          format-icons = {
-            headphone = "";
-            default = [ "" "" "" ];
-          };
-        };
-
-        network = {
-          format-ethernet = "<span color='#06b6d4'>󰈁</span> | <span color='#ea580c'></span> {bandwidthUpBytes}  <span color='#ea580c'></span> {bandwidthDownBytes}";
-          format-wifi = "<span color='#06b6d4'>{icon}</span> | <span color='#ea580c'></span> {bandwidthUpBytes}  <span color='#ea580c'></span> {bandwidthDownBytes}";
-          format-disconnected = "<span color='#b91c1c'>󰈂</span>";
-          format-icons = [ "󰤟" "󰤢" "󰤥" "󰤨" ];
-          interval = 5;
-        };
-
-        "sway/workspaces" = {
-          disable-scroll = true;
-          all-outputs = true;
-        };
-      };
     };
     systemd = {
       enable = true;
