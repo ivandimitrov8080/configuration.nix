59d34c8182a665e579093a815e04b7f57b69f0f0
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-06-21 19:55:57 +0200

    remove waybar calendar tooltip

--- home/modules/programs/waybar/default.nix
+++ home/modules/programs/waybar/default.nix
@@ -5,19 +5,6 @@
     settings = {
       mainBar =
         let
-          calendar = {
-            mode = "year";
-            mode-mon-col = 3;
-            weeks-pos = "right";
-            format = {
-              months = "<span color='#ffead3'><b>{}</b></span>";
-              days = "<span color='#ecc6d9'><b>{}</b></span>";
-              weeks = "<span color='#99ffdd'><b>W{}</b></span>";
-              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
-              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
-            };
-          };
-          tooltip-format = "<tt><small>{calendar}</small></tt>";
         in
         {
           layer = "top";
@@ -34,20 +21,14 @@
           "clock#time" = {
             format = "{:%H:%M:%S}";
             interval = 1;
-            tooltip-format = tooltip-format;
-            calendar = calendar;
           };
 
           "clock#week" = {
             format = "{:%a}";
-            tooltip-format = tooltip-format;
-            calendar = calendar;
           };
 
           "clock#year" = {
             format = "{:%Y-%m-%d}";
-            tooltip-format = tooltip-format;
-            calendar = calendar;
           };
 
           battery = {
