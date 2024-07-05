a1ed6a69fa433c11ee3e7c2fe9489f5c565f2dcd
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-07-05 22:07:52 +0200

    create packages for overlays

--- default.nix
+++ default.nix
@@ -2,19 +2,34 @@ top@{ inputs, ... }: {
   imports = [ ./nixos ./home ];
   systems = [ "x86_64-linux" ];
   flake.stateVersion = "24.05";
-  perSystem = perSystem@{ system, ... }: {
+  perSystem = perSystem@{ config, system, pkgs, ... }: {
+    config.packages = {
+      nvim = inputs.ide.nvim.${system}.standalone.default {
+        autoCmd = [
+          {
+            callback.__raw = /*lua*/ '' 
+              function() require("otter").activate() end 
+            '';
+            event = [ "BufEnter" "BufWinEnter" "BufWritePost" ];
+            pattern = [ "*.nix" ];
+          }
+        ];
+        plugins.lsp.servers = {
+          bashls.enable = true;
+          pylsp.enable = true;
+          lua-ls.enable = true;
+        };
+        extraPlugins = with pkgs.vimPlugins; [ otter-nvim ];
+      };
+      scripts = (pkgs.buildEnv { name = "scripts"; paths = [ ./. ]; });
+    };
     config._module.args = {
       pkgs = import inputs.nixpkgs {
         inherit system;
         overlays = [
           (final: prev: {
-            nvim = inputs.ide.nvim.${system}.standalone.default {
-              plugins.lsp.servers = {
-                bashls.enable = true;
-                nushell.enable = true;
-              };
-            };
-            scripts = (prev.buildEnv { name = "scripts"; paths = [ ./. ]; });
+            nvim = config.packages.nvim;
+            scripts = config.packages.scripts;
           })
           inputs.sal.overlays.default
         ];
