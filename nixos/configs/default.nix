6d0aefdc94b3ee76e04195e0b8e5275022de78bc
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-07-06 13:07:59 +0200

    cleanup hardware

--- nixos/configs/default.nix
+++ nixos/configs/default.nix
@@ -2,7 +2,33 @@ toplevel@{ inputs, withSystem, ... }:
 let
   system = "x86_64-linux";
   nixosModules = toplevel.config.flake.nixosModules;
-  minimal = [ ./nova-hardware.nix inputs.hosts.nixosModule ] ++ (with nixosModules; [ grub base sound wayland security ivand wireless wireguard ]);
+  hardwareConfigurations = {
+    nova = { config, lib, modulesPath, ... }: {
+      imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+      boot = {
+        initrd = {
+          availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
+          kernelModules = [ ];
+          luks.devices."nixos".device = "/dev/disk/by-uuid/712dd8ba-d5b4-438a-9a77-663b8c935cfe";
+        };
+        kernelModules = [ "kvm-intel" ];
+        extraModulePackages = [ ];
+      };
+
+      fileSystems = {
+        "/" = { device = "/dev/disk/by-uuid/47536cbe-7265-493b-a2e3-bbd376a6f9af"; fsType = "btrfs"; };
+        "boot" = { device = "/dev/disk/by-uuid/4C3C-993A"; fsType = "vfat"; };
+      };
+      swapDevices = [ ];
+      networking.useDHCP = lib.mkDefault true;
+      # networking.interfaces.enp0s20f0u6.useDHCP = lib.mkDefault true;
+      # networking.interfaces.enp47s0.useDHCP = lib.mkDefault true;
+      # networking.interfaces.wlp45s0.useDHCP = lib.mkDefault true;
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    };
+  };
+  minimal = [ hardwareConfigurations.nova inputs.hosts.nixosModule ] ++ (with nixosModules; [ grub base sound wayland security ivand wireless wireguard ]);
 in
 {
   flake.nixosConfigurations = {
