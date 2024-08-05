f1b2a18a1d19be3f022f6386b0a3d7c39c52ff79
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-08-05 11:12:19 +0200

    add telegram

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -179,7 +179,7 @@ toplevel@{ moduleWithSystem, ... }: {
       top@{ ... }:
       perSystem@{ pkgs, config, ... }: {
         home = {
-          packages = with pkgs; [ audacity gimp grim libnotify libreoffice-qt mupdf slurp transmission_4 wl-clipboard xdg-user-dirs xdg-utils xwayland ];
+          packages = with pkgs; [ audacity gimp grim libnotify libreoffice-qt mupdf slurp transmission_4 wl-clipboard xdg-user-dirs xdg-utils xwayland kotatogram-desktop ];
           pointerCursor = with pkgs; { name = "catppuccin-mocha-green-cursors"; package = catppuccin-cursors.mochaGreen; size = 24; gtk.enable = true; };
         };
         wayland.windowManager.sway = {
