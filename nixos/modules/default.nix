d89757afb9c37cb0dc75b2221887e10563d0c48a
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-08-05 18:57:10 +0200

    rest of mailserver

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -252,7 +252,6 @@ top@{ inputs, moduleWithSystem, ... }: {
     vps = moduleWithSystem (toplevel@{ ... }: perSystem@{ ... }: {
       imports = [
         inputs.vpsadminos.nixosConfigurations.container
-        ../../hosts/vps/mailserver
       ];
     });
     mailserver = moduleWithSystem (toplevel@{ ... }: perSystem@{ config, pkgs, ... }: {
@@ -514,6 +513,55 @@ top@{ inputs, moduleWithSystem, ... }: {
             iptables -D INPUT -m tcp -p tcp --dport 22 -j vpn
             iptables -X vpn
           '';
+        };
+      };
+    });
+    rest = moduleWithSystem (toplevel@{ ... }: perSystem@{ pkgs, ... }: {
+      fileSystems."/mnt/export1981" = {
+        device = "172.16.128.47:/nas/5490";
+        fsType = "nfs";
+        options = [ "nofail" ];
+      };
+      users = {
+        users.ivand = {
+          isNormalUser = true;
+          hashedPassword =
+            "$2b$05$hPrPcewxj4qjLCRQpKBAu.FKvKZdIVlnyn4uYsWE8lc21Jhvc9jWG";
+          extraGroups = [ "wheel" "adm" "mlocate" ];
+          openssh.authorizedKeys.keys = [
+            ''
+              ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcLkzuCoBEg+wq/H+hkrv6pLJ8J5BejaNJVNnymlnlo ivan@idimitrov.dev
+            ''
+          ];
+        };
+        extraGroups = { mlocate = { }; };
+      };
+      services = {
+        openssh = {
+          enable = true;
+          settings = {
+            PermitRootLogin = "prohibit-password";
+          };
+        };
+      };
+      systemd = {
+        timers = {
+          bingwp = {
+            wantedBy = [ "timers.target" ];
+            timerConfig = {
+              OnCalendar = "*-*-* 10:00:00";
+              Persistent = true;
+            };
+          };
+        };
+        services = {
+          bingwp = {
+            description = "Download bing image of the day";
+            script = ''
+              ${pkgs.nushell}/bin/nu -c "http get ('https://bing.com' + ((http get https://www.bing.com/HPImageArchive.aspx?format=js&n=1).images.0.url)) | save  ('/var/pic' | path join ( [ (date now | format date '%Y-%m-%d'), '.png' ] | str join ))"
+              ${pkgs.nushell}/bin/nu -c "${pkgs.toybox}/bin/ln -sf (ls /var/pic | where type == file | get name | sort | last) /var/pic/latest.png"
+            '';
+          };
         };
       };
     });
