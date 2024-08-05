0b8e88f09d66568fda05c56e331e1882f9c4f6bb
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-08-05 18:54:30 +0200

    firewall module

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -484,5 +484,38 @@ top@{ inputs, moduleWithSystem, ... }: {
         };
       };
     });
+    firewall = moduleWithSystem (toplevel@{ ... }: perSystem@{ lib, ... }: {
+      networking = {
+        firewall = lib.mkForce {
+          enable = true;
+          allowedTCPPorts = [
+            25 # smtp
+            465 # smtps
+            80 # http
+            443 # https
+          ];
+          allowedUDPPorts = [
+            25
+            465
+            80
+            443
+            51820 # wireguard
+          ];
+          extraCommands = ''
+            iptables -N vpn  # create a new chain named vpn
+            iptables -A vpn --src 10.0.0.2 -j ACCEPT  # allow
+            iptables -A vpn --src 10.0.0.3 -j ACCEPT  # allow
+            iptables -A vpn --src 10.0.0.4 -j ACCEPT  # allow
+            iptables -A vpn -j DROP  # drop everyone else
+            iptables -I INPUT -m tcp -p tcp --dport 22 -j vpn
+          '';
+          extraStopCommands = ''
+            iptables -F vpn
+            iptables -D INPUT -m tcp -p tcp --dport 22 -j vpn
+            iptables -X vpn
+          '';
+        };
+      };
+    });
   };
 }
