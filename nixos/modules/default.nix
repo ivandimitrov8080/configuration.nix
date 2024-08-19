7df03517211e83fdfddbd6b2b2dc63dad9f3bc75
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-08-19 12:44:18 +0200

    wg

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -156,7 +156,7 @@ top @ { inputs, moduleWithSystem, ... }: {
           peers = [
             {
               publicKey = "5FiTLnzbgcbgQLlyVyYeESEd+2DtwM1JHCGz/32UcEU=";
-              allowedIPs = [ "10.0.0.1/32" ];
+              allowedIPs = [ "0.0.0.0/0" "::/0" ];
               endpoint = "37.205.13.29:51820";
               persistentKeepalive = 25;
             }
