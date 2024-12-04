543ad8e2bbf8bbbd32daf4e1ea9a5ea5f865e8fb
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-12-04 17:33:54 +0100

    Revert "update"
    This reverts commit 7f7314bf361f533eb1c9ef5a05a2869d5014f61a.

--- src/modules/nixos/default.nix
+++ src/modules/nixos/default.nix
@@ -20,7 +20,7 @@ top @ { inputs, moduleWithSystem, ... }: {
             };
           efi.canTouchEfiVariables = true;
         };
-        kernelPackages = pkgs.lib.mkDefault pkgs.linuxPackages_latest-libre;
+        kernelPackages = pkgs.lib.mkDefault pkgs.linuxPackages-libre;
       };
     });
     base = moduleWithSystem (_: { pkgs, ... }: {
@@ -300,7 +300,7 @@ top @ { inputs, moduleWithSystem, ... }: {
             ];
           };
         };
-        fonts.packages = with pkgs; [ nerd-fonts.fira-code noto-fonts noto-fonts-emoji noto-fonts-lgc-plus ];
+        fonts.packages = with pkgs; [ (nerdfonts.override { fonts = [ "FiraCode" ]; }) noto-fonts noto-fonts-emoji noto-fonts-lgc-plus ];
         users = {
           users = {
             ivand = {
