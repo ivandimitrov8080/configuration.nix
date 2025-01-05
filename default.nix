936d6c03d46b2128b418ac1d9e6116920145c373
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-01-05 15:19:59 +0100

    clean

--- default.nix
+++ default.nix
@@ -1,12 +1,3 @@
-top@{ inputs, ... }:
 {
   imports = [ ./src ];
-  systems = [ "x86_64-linux" ];
-  flake.stateVersion = "24.11";
-  perSystem =
-    { system, ... }:
-    {
-      config._module.args = {
-      };
-    };
 }
