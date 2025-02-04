f85c0e4c836cf414a931178e82c439274716fbc8
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-02-04 20:57:01 +0100

    add more memory to vm

---
+++ src/modules/nixos/virtualization/default.nix
@@ -0,0 +1,9 @@
+{
+  virtualisation.vmVariant = {
+    # following configuration is added only when building VM with build-vm
+    virtualisation = {
+      memorySize = 16384;
+      cores = 4;
+    };
+  };
+}
