f1e0bcf7e7a7da221e2991cef8b66a44987d2b0b
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-02-09 19:57:51 +0100

    graphical boot module

--- src/modules/nixos/boot/loader/grub/default.nix
+++ src/modules/nixos/boot/loader/grub/default.nix
@@ -1,25 +1,6 @@
 { pkgs, ... }:
 {
   boot = {
-    initrd.systemd.enable = true;
-    plymouth = {
-      enable = true;
-      theme = "rings";
-      themePackages = with pkgs; [
-        adi1090x-plymouth-themes
-      ];
-    };
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
     loader = {
       grub =
         let
---
+++ src/modules/nixos/graphical-boot/default.nix
@@ -0,0 +1,43 @@
+{ lib, config, ... }:
+let
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    ;
+  cfg = config.graphicalBoot;
+in
+{
+  options.graphicalBoot = {
+    enable = mkEnableOption "enable graphical boot config";
+  };
+
+  config = mkIf cfg.enable {
+    services.greetd = {
+      enable = true;
+    };
+    programs.regreet = {
+      enable = true;
+    };
+    boot = {
+      initrd.systemd.enable = true;
+      plymouth = {
+        enable = true;
+        theme = "rings";
+        themePackages = with pkgs; [
+          adi1090x-plymouth-themes
+        ];
+      };
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+      ];
+    };
+  };
+}
