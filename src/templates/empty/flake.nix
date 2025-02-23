b6ac21d780a5a884019efd696ba5c1c009a024f3
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-02-23 17:41:43 +0100

    default template add more

--- src/templates/empty/flake.nix
+++ src/templates/empty/flake.nix
@@ -2,7 +2,9 @@
   inputs = {
     configuration.url = "github:ivandimitrov8080/configuration.nix";
     nixpkgs.follows = "configuration/nixpkgs";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
   };
   outputs =
-    inputs: inputs.configuration.inputs.parts.lib.mkFlake { inherit inputs; } { imports = [ ./. ]; };
+    inputs: inputs.configuration.inputs.parts.lib.mkFlake { inherit inputs; } { imports = [ ./parts ]; };
 }
---
+++ src/templates/empty/parts/default.nix
@@ -0,0 +1,52 @@
+{ inputs, ... }:
+{
+  systems = [ "x86_64-linux" ];
+  perSystem =
+    { system, pkgs, ... }:
+    {
+      config = {
+        _module.args = {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.configuration.overlays.default
+              (final: prev: {
+                nvim = (
+                  prev.nvim.extend {
+                    plugins = {
+                      lsp.servers = {
+                        # pylsp.enable = true;
+                        # svelte.enable = true;
+                        # html.enable = true;
+                        # ts_ls.enable = true;
+                        # jsonls.enable = true;
+                        # tailwindcss.enable = true;
+                        # cssls.enable = true;
+                        # rust_analyzer = {
+                        #   installCargo = false;
+                        #   installRustc = false;
+                        # };
+                        # rustaceanvim = {
+                        #   enable = true;
+                        # };
+                      };
+                    };
+                  }
+                );
+              })
+            ];
+          };
+        };
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nvim
+            ];
+          };
+        };
+        packages = {
+          default = pkgs.stdenv.mkDerivation { };
+        };
+      };
+    };
+}
