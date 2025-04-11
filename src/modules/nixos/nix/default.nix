8d0368b03571507a403cd3ef16939c9ea777137f
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-04-11 21:38:39 +0200

    auto-optimize nix store

--- src/modules/nixos/nix/default.nix
+++ src/modules/nixos/nix/default.nix
@@ -1,9 +1,12 @@
 _: {
   nix = {
     extraOptions = ''experimental-features = nix-command flakes'';
-    settings.trusted-users = [
-      "root"
-      "@wheel"
-    ];
+    settings = {
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+      auto-optimise-store = true;
+    };
   };
 }
