34b4ab541b6d688c3960c7c7237b8483f71c09f3
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-04-26 00:36:23 +0200

    git user use default shell

--- src/modules/nixos/vps/default.nix
+++ src/modules/nixos/vps/default.nix
@@ -80,6 +80,7 @@ in
           ];
         };
         git = {
+          useDefaultShell = true;
           openssh.authorizedKeys.keys = [
             ''
               ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcLkzuCoBEg+wq/H+hkrv6pLJ8J5BejaNJVNnymlnlo ivan@idimitrov.dev
