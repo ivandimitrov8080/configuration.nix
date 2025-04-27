06e2a6e84389a9ea340dce8e2f4b95e9d1976d55
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-04-27 10:21:00 +0200

    cleanup users

--- src/modules/nixos/default.nix
+++ src/modules/nixos/default.nix
@@ -82,39 +82,6 @@ top@{ inputs, moduleWithSystem, ... }:
                 reminders
               ];
             };
-          users.test =
-            { ... }:
-            {
-              imports = with top.config.flake.homeManagerModules; [
-                base
-                shell
-                util
-                swayland
-                web
-                reminders
-                {
-                  home = {
-                    username = "test";
-                    homeDirectory = "/home/test";
-                    sessionVariables = {
-                      EDITOR = "nvim";
-                    };
-                    packages = with pkgs; [ nvim ];
-                    file = {
-                      ".w3m/config".text = ''
-                        inline_img_protocol 4
-                        imgdisplay kitty
-                        confirm_qq 0
-                        extbrowser ${pkgs.firefox}/bin/firefox
-                      '';
-                      ".w3m/keymap".text = ''
-                        keymap M EXTERN_LINK
-                      '';
-                    };
-                  };
-                }
-              ];
-            };
         };
         i18n.supportedLocales = [ "all" ];
         time.timeZone = "Europe/Prague";
@@ -144,25 +111,6 @@ top@{ inputs, moduleWithSystem, ... }:
                 "wheel"
               ];
               hashedPassword = "$y$j9T$Wf9ljhi4c.LUoX/LJEll//$cTP..D/lBWq1PPCzaHhym8V.cibPTjy2JvRYLTf5SZ7";
-            };
-            test = {
-              isNormalUser = true;
-              createHome = true;
-              extraGroups = [
-                "adbusers"
-                "adm"
-                "audio"
-                "bluetooth"
-                "dialout"
-                "input"
-                "kvm"
-                "mlocate"
-                "realtime"
-                "render"
-                "video"
-                "wheel"
-              ];
-              hashedPassword = "$y$j9T$SRmJu3w8Zm3iBavwpV/wi1$HLAGev7SkLvSlFki09UY6PQ90LOEO3qonmV6ZFgHCm4";
             };
           };
           extraGroups = {
