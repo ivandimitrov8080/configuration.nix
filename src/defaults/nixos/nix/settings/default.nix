4cec4f8aef00ece467eb8d6e80bed272d4f14c9c
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-05-05 11:16:07 +0200

    fix trusted-users name

--- src/defaults/nixos/nix/settings/default.nix
+++ src/defaults/nixos/nix/settings/default.nix
@@ -3,6 +3,6 @@ let
   inherit (lib) mkDefault;
 in
 {
-  nix.settings.trustedUsers = mkDefault [ "@wheel" ];
+  nix.settings.trusted-users = mkDefault [ "@wheel" ];
   nix.settings.auto-optimise-store = mkDefault true;
 }
