68385508f1f25939b88f9eecbef3ef602466a35f
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-06-07 01:09:56 +0200

    use kmscon

--- src/nixos/modules/meta/graphicalBoot/default.nix
+++ src/nixos/modules/meta/graphicalBoot/default.nix
@@ -20,6 +20,8 @@ in
     services.greetd.enable = true;
     services.greetd.settings.default_session.command =
       lib.mkDefault "${lib.getExe pkgs.greetd.tuigreet} --time --remember --theme 'text=#cdd6f4;time=#f9e2af;container=#1e1e2e;border=#45475a;title=#f5c2e7;greet=#f38ba8;prompt=#a6e3a1'";
+    services.kmscon.enable = true;
+    services.kmscon.hwRender = true;
     boot = {
       plymouth.enable = true;
       initrd.systemd.enable = true;
