01e2e7700d2e5cc81b04140e77fb2307b9783b25
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-07-10 21:25:02 +0200

    different repo

--- src/packages/ddlm/default.nix
+++ src/packages/ddlm/default.nix
@@ -7,7 +7,7 @@ rustPlatform.buildRustPackage {
   pname = "ddlm";
   version = "1.0";
   src = fetchFromGitHub {
-    owner = "deathowl";
+    owner = "ivandimitrov8080";
     repo = "ddlm";
     rev = "8a7213909c7a7f4672a6db05ca5fdd0b37c5ceeb";
     hash = "sha256-V3084fBpuCkJ9N0Rw6uBvjQPtZi2BXGxlvmEYH7RahE=";
