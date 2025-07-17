36151b4ac66500de0d72f655f164fe9e188c6ab1
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-07-17 21:34:39 +0200

    fix nginx settings

--- src/nixos/defaults/services/nginx/default.nix
+++ src/nixos/defaults/services/nginx/default.nix
@@ -6,7 +6,6 @@ in
   services.nginx.recommendedGzipSettings = mkDefault true;
   services.nginx.recommendedTlsSettings = mkDefault true;
   services.nginx.recommendedOptimisation = mkDefault true;
-  services.nginx.recommendedZstdSettings = mkDefault true;
   services.nginx.recommendedProxySettings = mkDefault true;
   services.nginx.recommendedUwsgiSettings = mkDefault true;
   services.nginx.recommendedBrotliSettings = mkDefault true;
