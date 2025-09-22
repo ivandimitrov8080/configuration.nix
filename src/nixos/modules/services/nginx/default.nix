86ca151dddd03fd8273a3b056b5b5967502939b0
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-09-22 19:51:26 +0200

    src proxy

--- src/nixos/modules/services/nginx/default.nix
+++ src/nixos/modules/services/nginx/default.nix
@@ -6,6 +6,21 @@
         client_header_timeout 5s;
       '';
       virtualHosts = {
+        "src.idimitrov.dev" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:9418";
+            extraConfig = ''
+              allow 10.0.0.0/8;
+              allow 192.168.0.0/8;
+              deny all;
+              proxy_http_version 1.1;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
+            '';
+          };
+        };
         "pic.idimitrov.dev" = {
           enableACME = true;
           forceSSL = true;
