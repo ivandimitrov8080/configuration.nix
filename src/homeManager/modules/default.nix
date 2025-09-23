04e977e5fac518ca200eadd94a2197495e3f546e
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-09-23 09:34:39 +0200

    fix warning

--- src/homeManager/modules/default.nix
+++ src/homeManager/modules/default.nix
@@ -41,7 +41,20 @@
   programs = {
     home-manager.enable = true;
     ssh = {
+      enableDefaultConfig = false;
       matchBlocks = {
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
         vpsfree-ivand = {
           hostname = "idimitrov.dev";
           user = "ivand";
