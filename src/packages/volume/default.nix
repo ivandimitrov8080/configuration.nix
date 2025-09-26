1702c446dd3d7ee217cc593f87514df45e7a052b
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-09-26 20:40:58 +0200

    fix volume script

--- src/packages/volume/default.nix
+++ src/packages/volume/default.nix
@@ -1,10 +1,21 @@
 {
   writers,
+  lib,
+  wireplumber,
   ...
 }:
 let
   inherit (builtins) readFile;
 in
-writers.writeNuBin "volume" ''
-  ${(readFile ./main.nu)}
-''
+writers.writeNuBin "volume"
+  {
+    makeWrapperArgs = [
+      "--prefix"
+      "PATH"
+      ":"
+      "${lib.makeBinPath [ wireplumber ]}"
+    ];
+  }
+  ''
+    ${(readFile ./main.nu)}
+  ''
