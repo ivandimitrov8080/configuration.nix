d176550c231fe3ad163ae50cd07c9f8d791b9f65
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-09-27 18:32:38 +0200

    same but in root

--- src/nixos/default.nix
+++ src/nixos/default.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 let
-  inherit (import ../lib { inherit lib; }) endsWith findDefaults;
+  inherit (import ../lib { inherit lib; }) endsWith findDefaults mkDefaultAttrs;
 in
-{
+mkDefaultAttrs {
   imports = with builtins; filter (x: !(endsWith "nixos/default.nix" x)) (findDefaults ./.);
 }
--- src/nixos/defaults/default.nix
+++
@@ -1,7 +0,0 @@
-{ lib, ... }:
-let
-  inherit (import ../../lib { inherit lib; }) endsWith findDefaults mkDefaultAttrs;
-in
-mkDefaultAttrs {
-  imports = with builtins; filter (x: !(endsWith "defaults/default.nix" x)) (findDefaults ./.);
-}
