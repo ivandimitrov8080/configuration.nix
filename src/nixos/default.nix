6398d0808c77aa0840e72a8a749cf436c230fa87
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-09-27 21:25:37 +0200

    if blesh set -o vi

--- src/nixos/default.nix
+++ src/nixos/default.nix
@@ -2,13 +2,14 @@
   pkgs,
   lib,
   options,
+  config,
   ...
 }:
 let
   inherit (import ../lib { inherit lib; }) findDefaults mkDefaultAttrs;
   mods = findDefaults ./modules;
   defs = builtins.map (
-    f: mkDefaultAttrs (import f { inherit pkgs lib options; })
+    f: mkDefaultAttrs (import f { inherit pkgs lib options config; })
   ) (findDefaults ./defaults);
 in
 {
--- src/nixos/defaults/programs/bash/default.nix
+++ src/nixos/defaults/programs/bash/default.nix
@@ -1,3 +1,10 @@
-_: {
+{ lib, config, ... }:
+let
+  cfg = config.programs.bash;
+in
+{
   programs.bash.blesh.enable = true;
+  programs.bash.interactiveShellInit = ''
+    ${lib.optionalString cfg.blesh.enable "set -o vi"}
+  '';
 }
