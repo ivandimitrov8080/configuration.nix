2dda14f6a41bf087a6f98954c1bed9afc8f3721a
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-10-25 20:47:30 +0200

    initial tests

--- src/test/flake.nix
+++ src/test/flake.nix
@@ -1,16 +1,21 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    configuration.url = "git+file://./../../flake.nix";
   };
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      configuration,
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
       checks.${system} = {
-        default = import ./wireguard { inherit pkgs; };
+        default = import ./wireguard { inherit pkgs configuration; };
       };
     };
 }
--- src/test/wireguard/default.nix
+++ src/test/wireguard/default.nix
@@ -1,17 +1,81 @@
-{ pkgs, ... }:
+{ pkgs, configuration, ... }:
+
+let
+  hubPub = builtins.readFile ./hub.pub;
+  spoke1Pub = builtins.readFile ./spoke1.pub;
+  spoke2Pub = builtins.readFile ./spoke2.pub;
+  mod = configuration.nixosModules.default;
+in
 pkgs.testers.runNixOSTest {
-  name = "test";
+  name = "wireguard-integration";
   nodes = {
-    machine =
+    hub =
       { pkgs, ... }:
       {
-        environment.systemPackages = [ pkgs.hello ];
+        imports = [ mod ];
+        meta.wireguard = {
+          enable = true;
+          isHub = true;
+          address = "10.0.0.1/24";
+          privateKeyFile = ./hub.priv;
+          peers = [
+            {
+              PublicKey = spoke1Pub;
+              AllowedIPs = [ "10.0.0.2/32" ];
+            }
+            {
+              PublicKey = spoke2Pub;
+              AllowedIPs = [ "10.0.0.3/32" ];
+            }
+          ];
+        };
+      };
+    spoke1 =
+      { pkgs, ... }:
+      {
+        imports = [ mod ];
+        meta.wireguard = {
+          enable = true;
+          isHub = false;
+          address = "10.0.0.2/24";
+          privateKeyFile = ./spoke1.priv;
+          peers = [
+            {
+              PublicKey = hubPub;
+              AllowedIPs = [ "10.0.0.1/32" ];
+              Endpoint = "hub:51820";
+            }
+          ];
+        };
+      };
+    spoke2 =
+      { pkgs, ... }:
+      {
+        imports = [ mod ];
+        meta.wireguard = {
+          enable = true;
+          isHub = false;
+          address = "10.0.0.3/24";
+          privateKeyFile = ./spoke2.priv;
+          peers = [
+            {
+              PublicKey = hubPub;
+              AllowedIPs = [ "10.0.0.1/32" ];
+              Endpoint = "hub:51820";
+            }
+          ];
+        };
       };
   };
-  testScript =
-    #py
-    ''
-      machine.wait_for_unit("multi-user.target");
-      machine.succeed("hello");
-    '';
+
+  testScript = ''
+    hub.wait_for_unit("network-online.target")
+    spoke1.wait_for_unit("network-online.target")
+    spoke2.wait_for_unit("network-online.target")
+
+    spoke1.succeed("ping -c1 10.0.0.1")
+    spoke2.succeed("ping -c1 10.0.0.1")
+    hub.succeed("ping -c1 10.0.0.2")
+    hub.succeed("ping -c1 10.0.0.3")
+  '';
 }
