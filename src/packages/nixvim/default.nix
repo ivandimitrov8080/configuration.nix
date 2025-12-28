b2c56fa39e5c1a52e60bb15f1cd638cdd54466fe
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-12-28 15:27:18 +0100

    nixvim: lean lang

--- src/packages/nixvim/default.nix
+++ src/packages/nixvim/default.nix
@@ -15,6 +15,42 @@ recurseIntoAttrs rec {
   haskell = main.extend {
     lsp.servers.hls.enable = true;
   };
+  lean = main.extend {
+    plugins.lean = {
+      enable = true;
+      settings = {
+        abbreviations = {
+          enable = true;
+          extra = {
+            wknight = "♘";
+          };
+        };
+        ft = {
+          default = "lean";
+          nomodifiable = [
+            "_target"
+          ];
+        };
+        infoview = {
+          horizontal_position = "top";
+          indicators = "always";
+          separate_tab = true;
+        };
+        lsp = {
+          enable = true;
+        };
+        mappings = false;
+        progress_bars = {
+          enable = false;
+        };
+        stderr = {
+          on_lines = {
+            __raw = "function(lines) vim.notify(lines) end";
+          };
+        };
+      };
+    };
+  };
   scala = main.extend {
     lsp.servers.metals.enable = true;
   };
