398286c3ae34d075351a9c7524a79dccd35ed5d1
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-03-25 17:40:59 +0100

    refactor modules

--- src/nixos/modules/virtualization/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  virtualisation.vmVariant = {
-    # following configuration is added only when building VM with build-vm
-    virtualisation = {
-      memorySize = 16384;
-      cores = 4;
-    };
-  };
-}
