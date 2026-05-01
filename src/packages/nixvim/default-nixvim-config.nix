b7e79dab85fd8672523ee882c758d7230ad78084
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-05-01 03:14:46 +0200

    nixvim: delete custom hover handler

--- src/packages/nixvim/default-nixvim-config.nix
+++ src/packages/nixvim/default-nixvim-config.nix
@@ -676,20 +676,6 @@ pkgs: {
                 end,
             })
         end
-        if client.server_capabilities.hoverProvider and vim.lsp.buf then
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.buf.with(vim.lsp.handlers.hover, {
-              border = "rounded",
-              max_width = 90,
-              max_height = 25,
-            })
-        end
-        if client.server_capabilities.signatureHelpProvider and vim.lsp.buf then
-            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.buf.with(vim.lsp.handlers.signature_help, {
-              border = "rounded",
-              max_width = 90,
-              max_height = 15,
-            })
-        end
       '';
     keymaps = [
       {
